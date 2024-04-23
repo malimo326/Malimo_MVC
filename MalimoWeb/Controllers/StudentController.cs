2f9cce34cc5e67dc0e9ec965896ce197c4ffdfa6
malimo326 <mahmoodali2965.ma@gmail.com>
2024-04-23 13:13:39 +0200

    Done

---
+++ MalimoWeb/Controllers/StudentController.cs
@@ -0,0 +1,83 @@
+﻿using MalimoWeb.Data;
+using MalimoWeb.Models;
+using MalimoWeb.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace MalimoWeb.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        public StudentController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed,
+            };
+
+            await dbContext.students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+           // return View();
+            return RedirectToAction("List", "Student");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.students.ToListAsync();
+            return View(students);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+          var student =  await dbContext.students.FindAsync(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.students.FindAsync(viewModel.Id);
+            if (student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+            }
+                return RedirectToAction("List", "Student");
+            
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.students.
+                AsNoTracking().
+                FirstOrDefaultAsync(x => x.Id == viewModel.Id);
+            if (student is not null)
+            {
+                dbContext.students.Remove(viewModel);
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Student");
+        }
+    }
+}
---
+++ MalimoWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MalimoWeb.Models;
+using MalimoWeb.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MalimoWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Student>students { get; set; }
+    }
+}
